2393ab66c30c7850f368bcf4e54abdfa51bee6f0
Sofiaevx <villanueva19.sofia@gmail.com>
2022-02-18 23:44:04 +0800

    Redesigning my project

--- Contact Tracing App for QR Code/Form1.Designer.cs
+++ Contact Tracing App for QR Code/Form1.Designer.cs
@@ -29,57 +29,61 @@ namespace Contact_Tracing_App_for_QR_Code
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CameraDisplay = new System.Windows.Forms.PictureBox();
+            this.ONOFFBtn = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraDisplay)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // CameraDisplay
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(240, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(318, 247);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.CameraDisplay.Location = new System.Drawing.Point(12, 12);
+            this.CameraDisplay.Name = "CameraDisplay";
+            this.CameraDisplay.Size = new System.Drawing.Size(250, 250);
+            this.CameraDisplay.TabIndex = 1;
+            this.CameraDisplay.TabStop = false;
+            // 
+            // ONOFFBtn
+            // 
+            this.ONOFFBtn.Location = new System.Drawing.Point(191, 268);
+            this.ONOFFBtn.Name = "ONOFFBtn";
+            this.ONOFFBtn.Size = new System.Drawing.Size(71, 32);
+            this.ONOFFBtn.TabIndex = 2;
+            this.ONOFFBtn.Text = "ON";
+            this.ONOFFBtn.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(210, 210);
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(249, 250);
+            this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(151, 228);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 271);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(275, 305);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ONOFFBtn);
+            this.Controls.Add(this.CameraDisplay);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Contact Tracing App";
+            ((System.ComponentModel.ISupportInitialize)(this.CameraDisplay)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox CameraDisplay;
+        private System.Windows.Forms.Button ONOFFBtn;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ Contact Tracing App for QR Code/Loading.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Contact_Tracing_App_for_QR_Code
+{
+    partial class Loading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loading));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Viewbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Scan Completed";
+ 
+            // 
+            // Viewbtn
+            // 
+            this.Viewbtn.Font = new System.Drawing.Font("Microsoft Yi Baiti", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Viewbtn.Location = new System.Drawing.Point(172, 118);
+            this.Viewbtn.Name = "Viewbtn";
+            this.Viewbtn.Size = new System.Drawing.Size(73, 34);
+            this.Viewbtn.TabIndex = 2;
+            this.Viewbtn.Text = "View";
+            this.Viewbtn.UseVisualStyleBackColor = true;
+            // 
+            // Loading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 164);
+            this.ControlBox = false;
+            this.Controls.Add(this.Viewbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Loading";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Viewbtn;
+    }
+}
